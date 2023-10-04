474b6831b0e001663571a15e611b86ef2b3ef510
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2023-10-04 22:41:30 +0200

    refactor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import cv2
-# import background_substraction as bs
-# import frame_differencing as fd
+import utils
+import background_substraction as bs
+import frame_differencing as fd
 import motion_history_image as mhi
 
 path = 'sequences/'
@@ -11,16 +12,16 @@ full_path = path + 'action/'
 # list all files in the directory
 files = os.listdir(full_path)
 
-def grayscaled_image(file):
-    return cv2.cvtColor(cv2.imread(full_path + file), cv2.COLOR_BGR2GRAY)
+frames = [utils.grayscaled_image(full_path + file) for file in files]
 
-frames = [grayscaled_image(file) for file in files]
+functions = [bs.background_substraction, fd.frame_differencing_average, mhi.mhi]
 
-# result = bs.background_substraction(frames)
-# result = fd.frame_differencing_average(frames)
-result = mhi.mhi(frames)
+results = [function(frames) for function in functions]
 
-cv2.imshow('result', result)
+for i in range(len(results)):
+    cv2.imshow('result', results[i])
+    cv2.setWindowTitle('result', functions[i].__name__)
+
 cv2.waitKey(0)
 
 
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 def compute_average_background(frames):
     average_background = np.zeros(frames[0].shape, dtype=np.double)
@@ -6,3 +7,6 @@ def compute_average_background(frames):
     np.add.reduce(frames, out=average_background)
 
     return average_background / len(frames)
+
+def grayscaled_image(path):
+    return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
