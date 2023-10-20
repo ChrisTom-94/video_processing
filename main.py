bfc04b517592252201fb6f04efd0d960c5f39bbd
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2023-10-20 22:07:06 +0200

    fix issues that sequences files were loaded in the wrong order

--- main.py
+++ main.py
@@ -3,32 +3,20 @@ import video_processing as vp
 
 DIR = "results/"
 
-exercices = [
-    # question 1.1
-    # (a)
-    # [['course', 'neige'], [vp.background_substraction]],
-    # (b)
+comparisons = [
+    [['course', 'neige'], [vp.background_substraction]],
     # [['action', 'pirate'], [vp.background_substraction]],
-    # question 1.2
-    # (a)
     # [['action', 'femme'], [vp.frame_differencing]],
-    # (b)
     # [['course', 'neige'], [vp.frame_differencing]],
-    # question 1.3
-    # (a) (b)
-    [['action', 'course', 'femme'], [vp.mhi]],
-    # (c)
+    # [['action', 'course', 'femme'], [vp.mhi]],
     # [['pirate'], [vp.mhi]],
-    # question 1.4
     # [['homme', 'pirate'], [vp.mhi, vp.background_substraction, vp.frame_differencing]],
-    # question 1.5
     # [['toupie'], [vp.mhi, vp.background_substraction, vp.frame_differencing]],
-    # question 1.6
     # [['lumiere'], [vp.mhi, vp.background_substraction, vp.frame_differencing]],
 ]
 
-for sequence_names, functions in exercices:
+for sequence_names, functions in comparisons:
     for fn in functions:
         sequences = utils.retrieve_frames(sequence_names)
-        results = utils.compareSequences(fn, sequences)
-        utils.show_results(results, sequence_names, fn.__name__)
+        results = [fn(sequence) for sequence in sequences]
+        utils.display(results, sequence_names, fn.__name__)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import os
+import re
 
 DIR = 'sequences/'
 
@@ -8,7 +9,7 @@ def abs_diff(frame1, frame2):
     return np.double(cv2.absdiff(frame1, frame2))
 
 def normalize(input):
-    M = np.max(input)
+    M = np.max(np.max(input))
     return np.uint8(255 * input / M)
 
 def compute_average_background(frames):
@@ -19,20 +20,21 @@ def compute_average_background(frames):
 def grayscaled_image(path):
     return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
 
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(data, key=alphanum_key)
+
 def retrieve_frames(paths):
-    return [[grayscaled_image(full_path + '/' + file) for file in files] 
+    return [np.array([grayscaled_image(full_path + '/' + file) for file in sorted_alphanumeric(files)]) 
                 for full_path, _, files in os.walk(DIR) 
                     if len(files) > 0 and full_path.split('/')[-1] in paths]
 
-def compareSequences(fn, sequences):
-    return [fn(np.array(sequence)) for sequence in sequences]
-
-def show_results(results, sequence_names, fn_name):
+def display(results, sequence_names, fn_name):
     for i in range(len(results)):
         name = sequence_names[i] + " - " + fn_name
         cv2.imshow(name, results[i])
         
-        #if file don't exist, create it
         if not os.path.exists("./results/" + name + ".jpg"):
             cv2.imwrite("./results/" + name + ".jpg", results[i])
     cv2.waitKey(0)
